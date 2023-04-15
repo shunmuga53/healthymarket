3ffd1f971425a1ad0986d459c6f95588aaa7f029
shunmuga53 <shunmugasundarambaskaran3@gmail.com>
2023-04-15 19:52:38 +0530

    created healthymarket application

---
+++ web.healthymarket/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace web.healthymarket.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
